9951a5d48a82b6531db1816fac0e4e65683833c8
rcuryr <renato.c.rodrigues@caixa.gov.br>
2018-10-01 13:13:41 -0300

    Add files via upload

---
+++ ambiente.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Definindo variaveis do ambiente
+set NODE_ENV=development
+Path=%Path%;C:\Users\c111348\Documents\Angular\desenv\MongoDB4;C:\Users\c111348\Documents\Angular\desenv\MongoDB4\bin;C:\Users\c111348\Documents\Angular\desenv\node-v8.11.3;C:\Users\c111348\Documents\Angular\desenv\node-v8.11.3\node_modules;
